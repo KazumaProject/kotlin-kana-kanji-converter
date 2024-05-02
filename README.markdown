57348cdcebc9096e6de8a264161bdfe73edba7e2
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2024-05-02 04:39:49 -0400

    refactor readme

--- README.markdown
+++ README.markdown
@@ -32,17 +32,17 @@ fun main() {
     val result2BestPath = kanaKanjiEngine.viterbiAlgorithm(word2)
     val result3BestPath = kanaKanjiEngine.viterbiAlgorithm(word3)
 
-    val result1AStarAlgorithm = kanaKanjiEngine.nBestPath(word1,5)
-    val result2AStarAlgorithm = kanaKanjiEngine.nBestPath(word2,5)
-    val result3AStarAlgorithm = kanaKanjiEngine.nBestPath(word3,5)
+    val result1NBest = kanaKanjiEngine.nBestPath(word1,5)
+    val result2NBest = kanaKanjiEngine.nBestPath(word2,5)
+    val result3NBest = kanaKanjiEngine.nBestPath(word3,5)
 
     println("Viterbi $word1 =>=> $result1BestPath")
     println("Viterbi $word2 =>=> $result2BestPath")
     println("Viterbi $word3 =>=> $result3BestPath")
 
-    println("nBestPath $word1 =>=> $result1AStarAlgorithm")
-    println("nBestPath $word2 =>=> $result2AStarAlgorithm")
-    println("nBestPath $word3 =>=> $result3AStarAlgorithm")
+    println("nBestPath $word1 =>=> $result1NBest")
+    println("nBestPath $word2 =>=> $result2NBest")
+    println("nBestPath $word3 =>=> $result3NBest")
 
 }
 ```
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -242,17 +242,17 @@ private fun testBestPath(){
     val result2BestPath = kanaKanjiEngine.viterbiAlgorithm(word2)
     val result3BestPath = kanaKanjiEngine.viterbiAlgorithm(word3)
 
-    val result1AStarAlgorithm = kanaKanjiEngine.nBestPath(word1,5)
-    val result2AStarAlgorithm = kanaKanjiEngine.nBestPath(word2,5)
-    val result3AStarAlgorithm = kanaKanjiEngine.nBestPath(word3,5)
+    val result1NBest = kanaKanjiEngine.nBestPath(word1,5)
+    val result2NBest = kanaKanjiEngine.nBestPath(word2,5)
+    val result3NBest = kanaKanjiEngine.nBestPath(word3,5)
 
     println("Viterbi $word1 =>=> $result1BestPath")
     println("Viterbi $word2 =>=> $result2BestPath")
     println("Viterbi $word3 =>=> $result3BestPath")
 
-    println("nBestPath $word1 =>=> $result1AStarAlgorithm")
-    println("nBestPath $word2 =>=> $result2AStarAlgorithm")
-    println("nBestPath $word3 =>=> $result3AStarAlgorithm")
+    println("nBestPath $word1 =>=> $result1NBest")
+    println("nBestPath $word2 =>=> $result2NBest")
+    println("nBestPath $word3 =>=> $result3NBest")
 
     println("time to find shortest path $word2: $time1")
     println("time to find nBest path $word2: $time2")
