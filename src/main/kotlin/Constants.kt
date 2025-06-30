62ef83a9a61ed21c8d7045b38297f731ed1306f2
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2025-06-30 17:49:05 -0400

    update dictionary

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -1861,7 +1861,7 @@ object Constants {
             yomi = "こわすぎ",
             leftId = 2391,
             rightId = 1949,
-            cost = 4000,
+            cost = 2500,
             tango = "怖すぎ"
         ),
         Dictionary(
