9f5a87760cb252529ed33de0e9665600270a7343
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2025-09-15 14:00:03 -0400

    update id numbers

--- src/main/kotlin/Other.kt
+++ src/main/kotlin/Other.kt
@@ -3,7 +3,7 @@ package com.kazumaproject
 import com.kazumaproject.graph.Node
 
 object Other {
-    const val NUM_OF_CONNECTION_ID = 2670
+    const val NUM_OF_CONNECTION_ID = 2671
     val BOS = Node(
         l = 0,
         r = 0,
