5d2e51ba3c4f83600427372241c1b79b9b151d9e
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2026-03-30 00:12:09 -0400

    Revert "Merge pull request #60 from KazumaProject/feature/create-test"
    This reverts commit 24b7b736dcff5272f42d689b6c20b097c87d204f, reversing
    changes made to a8a90631f1770dcd7a57e93cc453180e95f9b5b6.

--- src/main/kotlin/graph/GraphBuilder.kt
+++ src/main/kotlin/graph/GraphBuilder.kt
@@ -6,44 +6,19 @@ import com.kazumaproject.Other.BOS
 import com.kazumaproject.dictionary.TokenArray
 import com.kazumaproject.hiraToKata
 
-data class GraphDictionary(
-    val yomiTrie: LOUDSWithTermId,
-    val tangoTrie: LOUDS,
-    val tokenArray: TokenArray,
-)
-
 class GraphBuilder {
-
-    private fun addOrUpdateNode(
-        nodesByEndIndex: MutableMap<Int, MutableList<Node>>,
-        endIndex: Int,
-        newNode: Node,
-    ) {
-        val nodes = nodesByEndIndex.getOrPut(endIndex) { mutableListOf() }
-        val existingNodeIndex = nodes.indexOfFirst {
-            it.tango == newNode.tango && it.l == newNode.l && it.r == newNode.r
-        }
-
-        if (existingNodeIndex == -1) {
-            nodes.add(newNode)
-            return
-        }
-
-        if (newNode.score < nodes[existingNodeIndex].score) {
-            nodes[existingNodeIndex] = newNode
-        }
-    }
 
     fun constructGraph(
         str: String,
-        dictionaries: List<GraphDictionary>,
+        yomiTrie: LOUDSWithTermId,
+        tangoTrie: LOUDS,
+        tokenArray: TokenArray
     ): List<MutableList<MutableList<Node>>> {
-        val graph = MutableList(str.length + 2) { mutableListOf<MutableList<Node>>() }
-        val nodesByEndIndex = mutableMapOf<Int, MutableList<Node>>()
+        val graph: MutableList<MutableList<MutableList<Node>>?> = mutableListOf()
         for (i in 0 .. str.length + 1){
             when(i){
-                0 -> graph[i] = mutableListOf(mutableListOf(BOS))
-                str.length + 1 -> graph[i] = mutableListOf(
+                0 -> graph.add(i, mutableListOf(mutableListOf(BOS)))
+                str.length + 1 -> graph.add(i, mutableListOf(
                     mutableListOf(
                         Node(
                         l = 0,
@@ -56,45 +31,41 @@ class GraphBuilder {
                         sPos = str.length + 1
                     ))
                 )
+                )
+                else -> graph.add(i,null)
             }
         }
 
         for (i in str.indices){
             val subStr = str.substring(i, str.length)
-            dictionaries.forEach { dictionary ->
-                val commonPrefixSearch = dictionary.yomiTrie.commonPrefixSearch(subStr)
-                commonPrefixSearch.forEach { yomiStr ->
-                    val termId = dictionary.yomiTrie.getTermId(dictionary.yomiTrie.getNodeIndex(yomiStr))
-                    val listToken = dictionary.tokenArray.getListDictionaryByYomiTermId(termId)
-                    listToken.forEach {
-                        addOrUpdateNode(
-                            nodesByEndIndex = nodesByEndIndex,
-                            endIndex = i + yomiStr.length,
-                            newNode = Node(
-                                l = dictionary.tokenArray.leftIds[it.posTableIndex.toInt()],
-                                r = dictionary.tokenArray.rightIds[it.posTableIndex.toInt()],
-                                score = it.wordCost.toInt(),
-                                f = it.wordCost.toInt(),
-                                g = it.wordCost.toInt(),
-                                tango = when (it.nodeId) {
-                                    -2 -> yomiStr
-                                    -1 -> yomiStr.hiraToKata()
-                                    else -> dictionary.tangoTrie.getLetter(it.nodeId)
-                                },
-                                len = yomiStr.length.toShort(),
-                                sPos = i
-                            ),
-                        )
-                    }
+            val commonPrefixSearch = yomiTrie.commonPrefixSearch(subStr)
+            commonPrefixSearch.forEach { yomiStr ->
+                val termId = yomiTrie.getTermId(yomiTrie.getNodeIndex(yomiStr))
+                val listToken = tokenArray.getListDictionaryByYomiTermId(termId)
+                val tangoList = listToken.map {
+                    Node(
+                        l = tokenArray.leftIds[it.posTableIndex.toInt()],
+                        r = tokenArray.rightIds[it.posTableIndex.toInt()],
+                        score = it.wordCost.toInt(),
+                        f = it.wordCost.toInt(),
+                        g = it.wordCost.toInt(),
+                        tango = when (it.nodeId) {
+                            -2 -> yomiStr
+                            -1 -> yomiStr.hiraToKata()
+                            else -> tangoTrie.getLetter(it.nodeId)
+                        },
+                        len = yomiStr.length.toShort(),
+                        sPos = i
+                    )
+                }
+                if (graph[i + yomiStr.length].isNullOrEmpty()){
+                    graph[i + yomiStr.length] = mutableListOf(tangoList.toMutableList())
+                }else{
+                    graph[i + yomiStr.length]!!.add(tangoList.toMutableList())
                 }
             }
         }
-
-        nodesByEndIndex.forEach { (endIndex, nodes) ->
-            graph[endIndex] = mutableListOf(nodes.toMutableList())
-        }
-
-        return graph.toList()
+        return graph.toList().filterNotNull()
     }
 
 }
