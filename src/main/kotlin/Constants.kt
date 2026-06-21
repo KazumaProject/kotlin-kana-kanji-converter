d16413f077203cde6323d6caf7076c94cc53c52a
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2026-06-21 19:06:59 -0400

    refactor github actions

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -3397,6 +3397,27 @@ object Constants {
             cost = 7000,
             tango = "半固体"
         ),
+        Dictionary(
+            yomi = "かきじゅん",
+            leftId = `名詞,一般,*,*,*,*,*`,
+            rightId = `名詞,一般,*,*,*,*,*`,
+            cost = 5000,
+            tango = "書き順"
+        ),
+        Dictionary(
+            yomi = "えんみ",
+            leftId = `名詞,一般,*,*,*,*,*`,
+            rightId = `名詞,一般,*,*,*,*,*`,
+            cost = 5000,
+            tango = "塩味"
+        ),
+        Dictionary(
+            yomi = "しおみ",
+            leftId = `名詞,一般,*,*,*,*,*`,
+            rightId = `名詞,一般,*,*,*,*,*`,
+            cost = 6000,
+            tango = "塩味"
+        ),
     )
 
     val ADDS_NEW_WORDS = listOf(
